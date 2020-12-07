178ff421f2dcfa062a46ee8f904ee05598be6129
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2020-12-07 16:10:52 +0600

    Add files via upload
    fun way.

---
+++ calculate_age3.py
@@ -0,0 +1,12 @@
+# Python3 code to calculate age in years
+from datetime import date
+
+
+def calculateAge(birthDate):
+    days_in_year = 365.2425
+    age = int((date.today() - birthDate).days / days_in_year)
+    return age
+
+
+# Driver code
+print(calculateAge(date(1997, 2, 3)), "years")
