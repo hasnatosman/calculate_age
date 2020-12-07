f7f84b329f2c7be2ee431d13bfcef3ca9ad56566
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2020-12-07 16:09:56 +0600

    Add files via upload
    easy way.

---
+++ calculate_age1.py
@@ -0,0 +1,10 @@
+from datetime import date
+
+
+def calculateAge(birthDate):
+    today = date.today()
+    age = today.year - birthDate.year - ((today.month, today.day) < (birthDate.month, birthDate.day))
+    return age
+
+
+print(calculateAge(date(1995, 12, 3)), 'years')
